8997ae93cf6dee03c1337d645826899ea1e5eb85
Amir Savand <amir@savandbros.com>
2018-02-22 19:50:00 +0300

    Storage class for keeping data
    Fixes #38

--- Assets/Scripts/Storage.cs
+++ Assets/Scripts/Storage.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 public class Storage : MonoBehaviour
 {
 	// Coins are collected in game then added once player is dead
-	public int coins;
+	public static int coins;
 
 	// Highest score that player got in a single game
-	public int highScore;
+	public static int highScore;
+
+	void Awake ()
+	{
+		// Keep it between scenes
+		DontDestroyOnLoad (gameObject);
+
+		// Destroy it if it's a duplicate
+		if (FindObjectsOfType (GetType ()).Length > 1) {
+			Destroy (gameObject);
+		}
+	}
 }
