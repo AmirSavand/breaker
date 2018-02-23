8185d1773d9c8ee7999580418b5e094a51ece263
Amir Savand <amirm3hdi@gmail.com>
2018-02-24 00:53:51 +0300

    Move player when round starts
    Resolves #53

---
+++ Assets/Scripts/Components/MoveTo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTo : MonoBehaviour
+{
+	public Transform target;
+	public float speed = 1;
+	public float stopRange = 0.1f;
+	public bool destroyTarget = true;
+
+	void Update ()
+	{
+		// Check target
+		if (!target) {
+			return;
+		}
+			
+		// Move to position of target smoothly
+		transform.position = Vector2.Lerp (transform.position, target.position, Time.deltaTime * speed);
+
+		// Stop at a certain distance
+		if (Vector2.Distance (transform.position, target.position) <= stopRange) {
+
+			// Set position to target instantly
+			transform.position = target.position;
+
+			// Destroy target if should
+			if (destroyTarget) {
+				Destroy (target.gameObject);
+			}
+		}
+	}
+}
