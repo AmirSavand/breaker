baa961000debf2f84622247d24c07b555bda6466
Amir Savand <amirm3hdi@gmail.com>
2018-02-24 04:09:30 +0300

    Rock Stars break into star pieces too
    Resolves #52

--- Assets/Scripts/Components/MoveTo.cs
+++ Assets/Scripts/Components/MoveTo.cs
@@ -5,30 +5,64 @@ using UnityEngine;
 public class MoveTo : MonoBehaviour
 {
 	public Transform target;
+	public string targetTag;
 	public float speed = 1;
+	public float moveAfter = 0;
 	public float stopRange = 0.1f;
-	public bool destroyTarget = true;
+	public bool destroyTargetOnRange = true;
+	public bool destroySelfOnRange = false;
+	public bool resetVelocity = false;
+
+	private float startTime;
+
+	void OnEnable ()
+	{
+		// Store starting time for moveAfter
+		startTime = Time.time;
+
+		// If should find target with tag
+		if (!target && targetTag.Length > 0) {
+		
+			// Get target by tag
+			GameObject targetObject = GameObject.FindWithTag (targetTag);
+
+			// Found, store target
+			if (targetObject) {
+				target = targetObject.GetComponent<Transform> ();
+			}
+		}
+	}
 
 	void Update ()
 	{
-		// Check target
-		if (!target) {
+		// Check target and start time
+		if (!target || Time.time - startTime < moveAfter) {
 			return;
 		}
-			
+
+		// No gravity/rigidbody effect
+		if (resetVelocity) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 ();
+		}
+
 		// Move to position of target smoothly
 		transform.position = Vector2.Lerp (transform.position, target.position, Time.deltaTime * speed);
 
 		// Stop at a certain distance
 		if (Vector2.Distance (transform.position, target.position) <= stopRange) {
 
-			// Set position to target instantly
-			transform.position = target.position;
-
 			// Destroy target if should
-			if (destroyTarget) {
+			if (destroyTargetOnRange) {
 				Destroy (target.gameObject);
 			}
+
+			// Destroy self if should
+			if (destroySelfOnRange) {
+				Destroy (gameObject);
+			}
+
+			// Set position to target instantly
+			transform.position = target.position;
 		}
 	}
 }
