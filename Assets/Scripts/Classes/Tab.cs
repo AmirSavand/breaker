059f013a718742a3120f115b3f04ac5b4b1ef9b5
Amir Savand <amir@savandbros.com>
2018-04-07 22:07:19 +0300

    Add Tab class

---
+++ Assets/Scripts/Classes/Tab.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tab : MonoBehaviour
+{
+    public Button button;
+    public GameObject tabObject;
+    public Color activeColor;
+    public Color deactiveColor;
+    public bool isActive = false;
+
+    void Start ()
+    {
+        // Set onClick event
+        button.onClick.AddListener (activate);
+    }
+
+    public void activate ()
+    {
+        // Reset all other tabs first
+        Views.ResetTabs ();
+
+        // Activate tab
+        isActive = true;
+
+        // Set to active color
+        ColorBlock colors = button.colors;
+        colors.normalColor = activeColor;
+        colors.highlightedColor = activeColor;
+        button.colors = colors;
+
+        // Show tab object
+        if (tabObject) {
+            tabObject.SetActive (true);
+        }
+    }
+
+    public void deactivate ()
+    {
+        // Activate tab
+        isActive = false;
+
+        // Set to active color
+        ColorBlock colors = button.colors;
+        colors.normalColor = deactiveColor;
+        colors.highlightedColor = deactiveColor;
+        button.colors = colors;
+
+        // Show tab object
+        if (tabObject) {
+            tabObject.SetActive (false);
+        }
+    }
+}
+
