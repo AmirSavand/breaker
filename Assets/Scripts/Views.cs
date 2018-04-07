65a47647610c3d58e42603de23cb784076aa81df
Amir Savand <amir@savandbros.com>
2018-04-07 22:07:04 +0300

    Add View to manage views and tabs

---
+++ Assets/Scripts/Views.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Views : MonoBehaviour
+{
+    public static Tab[] Tabs;
+
+    void Start ()
+    {
+        // Get inits
+        Tabs = GetComponentsInChildren<Tab> ();
+    }
+
+    public static void ResetTabs ()
+    {
+        // Deactivate all tabs
+        foreach (Tab tab in Tabs) {
+            tab.deactivate ();
+        }
+    }
+}
