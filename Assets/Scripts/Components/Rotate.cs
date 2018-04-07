d8074a7825783da9d6f074de13512eb0429c2e40
Amir Savand <amir@savandbros.com>
2018-04-07 16:19:36 +0300

    Add Rotate component

---
+++ Assets/Scripts/Components/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public Vector3 direction;
+
+    public float speed = 100;
+
+    void Update ()
+    {
+        // Rotate
+        transform.Rotate (direction * speed * Time.deltaTime);
+    }
+}
