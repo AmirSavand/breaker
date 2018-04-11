d201897fc496431c451105f0df43af7269ba21c9
Amir Savand <amir@savandbros.com>
2018-04-11 13:47:00 +0300

    Rotate stops when button is disabled

--- Assets/Scripts/Components/Rotate.cs
+++ Assets/Scripts/Components/Rotate.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Rotate : MonoBehaviour
 {
+    public Button button;
     public Vector3 direction;
 
     public float speed = 100;
 
     void Update ()
     {
+        // Stop rotation if button is attached and disabled
+        if (button && !button.interactable) {
+            return;
+        }
+
         // Rotate
         transform.Rotate (direction * speed * Time.deltaTime);
     }
