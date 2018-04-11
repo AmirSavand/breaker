8f0643be335c6aba6f23120777b24a45d900b503
Amir Savand <amir@savandbros.com>
2018-04-11 15:56:48 +0300

    Remove world and use ranges to destroy objects via Destroy

--- Assets/Scripts/Components/Destroy.cs
+++ Assets/Scripts/Components/Destroy.cs
@@ -6,11 +6,37 @@ public class Destroy : MonoBehaviour
 {
     public bool destroyAfterParticle = false;
 
+    public bool destroyExitTriggers = false;
+    public string destroyExitTriggersTagOnly;
+
     void Start ()
     {
         // Destroy after particle duration
         if (destroyAfterParticle) {
             Destroy (gameObject, GetComponent<ParticleSystem> ().main.duration);
+        }
+    }
+
+    void OnTriggerExit2D (Collider2D other)
+    {
+        // Destroy everything that leaves
+        if (!destroyExitTriggers) {
+            return;
+        }
+
+        // Compare tag if set
+        if (destroyExitTriggersTagOnly.Length > 0 && !other.CompareTag (destroyExitTriggersTagOnly)) {
+            return;
+        }
+         
+        // Destroy self if has rb
+        if (other.GetComponent<Rigidbody2D> () != null) {
+            Destroy (other.gameObject);
+        }
+
+        // Destroy parent (rigidbody is there)
+        else {
+            Destroy (other.transform.parent.gameObject);
         }
     }
 }
