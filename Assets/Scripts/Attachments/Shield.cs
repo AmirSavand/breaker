9586d2147bfb1924a3be94fc5cf40e9c6a03a7a6
Amir Savand <amir@savandbros.com>
2018-04-13 16:59:07 +0300

    Add Sprites/Attachments folder

---
+++ Assets/Scripts/Attachments/Shield.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shield : MonoBehaviour
+{
+    public GameObject shieldObject;
+
+    public float shieldTime;
+
+    private Vector3 scaleTo;
+
+    void Start ()
+    {
+        scaleTo = transform.localScale;
+        transform.localScale = new Vector3 (0, 0, 1);
+    }
+
+    void Update ()
+    {
+        transform.localScale = Vector3.Lerp (transform.localScale, new Vector3 (1, 1, 1), 10 * Time.deltaTime);
+    }
+}
