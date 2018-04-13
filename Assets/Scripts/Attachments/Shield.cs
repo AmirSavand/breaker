8f184a4a3a5be713f33aae96eca5817f48ed73dc
Amir Savand <amir@savandbros.com>
2018-04-13 22:16:07 +0300

    Add Shield.energy

--- Assets/Scripts/Attachments/Shield.cs
+++ Assets/Scripts/Attachments/Shield.cs
@@ -8,6 +8,10 @@ public class Shield : MonoBehaviour
     public GameObject shield;
     public bool active = false;
 
+    public float energy;
+    public float maxEnergy = 10;
+    public float energyGainFactor = 1;
+
     private Hitpoint hitpoint;
     private Vector3 activeScale;
     private Vector3 deactiveScale;
@@ -15,6 +19,7 @@ public class Shield : MonoBehaviour
     void Awake ()
     {
         // Init vars
+        energy = maxEnergy;
         hitpoint = GetComponent<Hitpoint> ();
         activeScale = shield.transform.localScale;
         deactiveScale = new Vector3 (0, 0, 1);
@@ -28,13 +33,21 @@ public class Shield : MonoBehaviour
 
     void Update ()
     {
-        // Scale to active or deactive size
+        // If active and no energy, deactivate
+        if (active && energy == 0) {
+            active = false;
+        }
+
+        // Scale to (de)active size
         Vector3 scaleTo = active ? activeScale : deactiveScale;
 
-        // Smooth scaling
+        // Smooth scaling up/down
         shield.transform.localScale = Vector3.Lerp (shield.transform.localScale, scaleTo, 10 * Time.deltaTime);
 
-        // Make shit invulnerable
+        // Make ship (in)vulnerable
         hitpoint.isInvulnerable = active;
+
+        // Reduce/Gain energy
+        energy = Mathf.Clamp (energy + Time.deltaTime * (active ? -1 : energyGainFactor), 0, maxEnergy);
     }
 }
