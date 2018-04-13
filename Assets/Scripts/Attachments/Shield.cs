be66fd0dae8eeecfb5da09cc1adbcecdc30fc8a1
Amir Savand <amir@savandbros.com>
2018-04-13 18:02:49 +0300

    Add Shield (attachment script)

--- Assets/Scripts/Attachments/Shield.cs
+++ Assets/Scripts/Attachments/Shield.cs
@@ -5,20 +5,36 @@ using UnityEngine.UI;
 
 public class Shield : MonoBehaviour
 {
-    public GameObject shieldObject;
+    public GameObject shield;
+    public bool active = false;
 
-    public float shieldTime;
+    private Hitpoint hitpoint;
+    private Vector3 activeScale;
+    private Vector3 deactiveScale;
 
-    private Vector3 scaleTo;
-
-    void Start ()
+    void Awake ()
     {
-        scaleTo = transform.localScale;
-        transform.localScale = new Vector3 (0, 0, 1);
+        // Init vars
+        hitpoint = GetComponent<Hitpoint> ();
+        activeScale = shield.transform.localScale;
+        deactiveScale = new Vector3 (0, 0, 1);
+    }
+
+    void OnEnable ()
+    {
+        // Reset shield scale
+        shield.transform.localScale = new Vector3 (0, 0, 1);
     }
 
     void Update ()
     {
-        transform.localScale = Vector3.Lerp (transform.localScale, new Vector3 (1, 1, 1), 10 * Time.deltaTime);
+        // Scale to active or deactive size
+        Vector3 scaleTo = active ? activeScale : deactiveScale;
+
+        // Smooth scaling
+        shield.transform.localScale = Vector3.Lerp (shield.transform.localScale, scaleTo, 10 * Time.deltaTime);
+
+        // Make shit invulnerable
+        hitpoint.isInvulnerable = active;
     }
 }
