608b7040064cadc5235de0fee73e608fb093076a
Amir Savand <amir@savandbros.com>
2018-04-14 11:30:38 +0300

    Do not use slider for upgrades in shop (normal image)

--- Assets/Scripts/Views/Shop.cs
+++ Assets/Scripts/Views/Shop.cs
@@ -97,7 +97,7 @@ public class Shop : MonoBehaviour
 
         // Set price, bar and icon
         upgradeObject.GetComponentInChildren<Text> ().text = upgrade.getPrice ().ToString ();
-        upgradeObject.GetComponentInChildren<Slider> ().value = upgrade.getStockPercentage ();
+        upgradeObject.transform.Find ("Container/Icon/Level").GetComponent<Image> ().fillAmount = upgrade.getStockPercentage () * 100;
         upgradeObject.transform.Find ("Container/Icon").GetComponent<Image> ().sprite = upgrade.icon;
 
         // Shop the right button text
