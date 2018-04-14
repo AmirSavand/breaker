4c51fe11e29410e9592bc24083478180cbcea32d
Amir Savand <amir@savandbros.com>
2018-04-14 15:08:09 +0300

    Add Bonus class

---
+++ Assets/Scripts/Classes/Bonus.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum BonusType
+{
+    Consumable,
+    Duration
+}
+
+public class Bonus : MonoBehaviour
+{
+    public BonusType type;
+
+    public Color color;
+
+    public string title;
+
+    public float duration;
+
+    public float amount;
+}
