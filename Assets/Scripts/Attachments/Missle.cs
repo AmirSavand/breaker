c06436023088b64065debde3c46ee553d6a7d550
Amir Savand <amir@savandbros.com>
2018-07-01 19:23:25 +0400

    Add missle object

---
+++ Assets/Scripts/Attachments/Missle.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missle : MonoBehaviour
+{
+    public float missleSpeed = 3;
+    public float missleDamage = 200;
+    public float missleRate = 5;
+
+    public Transform missleFrom;
+    public GameObject missleObject;
+    public AudioSource missleSound;
+
+    private float lastTimeFired;
+
+    void Start ()
+    {
+        // Fire every missleRate seconds
+        InvokeRepeating ("fire", missleRate, missleRate);
+    }
+
+    /**
+     * Attempt to launch a missle
+     */
+    public void fire ()
+    {
+        // Check cooldown
+        if (Time.time - lastTimeFired < missleRate) {
+            return;
+        }
+
+        // Fire sound
+        missleSound.Play ();
+
+        // Create missle
+        GameObject instance = Instantiate (missleObject, missleFrom.transform.position, transform.rotation);
+
+        // Get damage component
+        Damage instanceDamage = instance.GetComponent<Damage> ();
+
+        // Set missle damage
+        instanceDamage.damage = missleDamage;
+
+        // Add ship hitpoints to white list of missle damage
+        instanceDamage.whiteList.AddRange (GetComponentsInChildren<Collider2D> ());
+
+        // Set missle speed
+        instance.GetComponent<MoveTo> ().speed = missleSpeed;
+
+        // Fire rate cooldown (save last time)
+        lastTimeFired = Time.time;
+    }
+}
+
